96bdeb310feb4c627e5f9a5c5e107181ca13cc0b
husam595habash <husam595habash@gmail.ccom>
2025-08-10 15:28:13 +0300

    splash screen

---
+++ app/src/main/java/com/example/apitest/data/api/SearchApiServices.kt
@@ -0,0 +1,4 @@
+package com.example.apitest.data.api
+
+class SearchApiServices {
+}
---
+++ app/src/main/java/com/example/apitest/ui/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.apitest.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.airbnb.lottie.LottieAnimationView
+import com.example.apitest.R
+
+class MainActivity : AppCompatActivity() {
+    lateinit var lottieLogo: LottieAnimationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lottieLogo = findViewById(R.id.splashscreen_icon_view)
+    }
+}
